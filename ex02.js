d955b236a9f9a4ce751530ab4b6294d41c39d205
siwoo <parksiwoo1214@naver.com>
2024-11-02 02:23:52 +0900

    second commit

---
+++ ex02.js
@@ -0,0 +1,18 @@
+let randomNumber = Math.floor(Math.random()*100)+1;
+
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
+
+let guessCount = 1;
+let resetButton;
+
+function checkGuess(){
+    alert("I am a placeholder~");
+}
+
+const myname = "siwoo park";
+const myname1 = `내 이름은 ${myname}`;
